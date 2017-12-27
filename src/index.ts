d2c6588ff845a799ba1101b745021c9c977ddbd6
Björn Andersson <bjorn.olof.andersson@gmail.com>
2017-12-27 15:37:02 +0100

    Export RouterFactoryProvider

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import {component} from '@ziggurat/tiamat';
 import {HttpServerFactory} from './factories/http';
 import {SocketIOServerFactory} from './factories/socket';
 
-export {MiddlewareProvider, RouterProvider, router, get} from './decorators';
+export {MiddlewareProvider, RouterFactoryProvider, RouterProvider,
+  router, get} from './decorators';
 export {RouterFactory} from './factories/router';
 export {ServerFactory} from './factories/server';
 
