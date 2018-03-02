0b21909a0686d4ec797cef624c54022a5f84bf98
Björn Andersson <bjorn.olof.andersson@gmail.com>
2018-03-02 17:46:08 +0100

    Added documentation to decorators

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -5,30 +5,76 @@ import {MiddlewareDecorator} from './middleware';
 import {RouterMeta} from './meta';
 import * as express from 'express';
 
+/**
+ * Router-level middleware.
+ *
+ * This decorator can be used to attach middleware to a router or server by decorating
+ * the factory.
+ */
 export const middleware = classDecorator<MiddlewareConfig[]>(
   new MiddlewareDecorator());
 
+/**
+ * HTTP GET request handler.
+ *
+ * Decorate a router-method to turn it into a request handler.
+ * Takes path as argument.
+ */
 export const get = propertyDecorator<string>(
   new RouterMethodDecorator('get'));
 
+/**
+ * HTTP POST request handler.
+ *
+ * Decorate a router-method to turn it into a request handler.
+ * Takes path as argument.
+ */
 export const post = propertyDecorator<string>(
   new RouterMethodDecorator('post'));
 
+/**
+ * HTTP PUT request handler.
+ *
+ * Decorate a router-method to turn it into a request handler.
+ * Takes path as argument.
+ */
 export const put = propertyDecorator<string>(
   new RouterMethodDecorator('put'));
 
+/**
+ * HTTP PATCH request handler.
+ *
+ * Decorate a router-method to turn it into a request handler.
+ * Takes path as argument.
+ */
 export const patch = propertyDecorator<string>(
   new RouterMethodDecorator('patch'));
 
+/**
+ * HTTP DELETE request handler.
+ *
+ * Decorate a router-method to turn it into a request handler.
+ * Takes path as argument.
+ */
 export const del = propertyDecorator<string>(
   new RouterMethodDecorator('delete'));
 
+/**
+ * Method-level middleware
+ *
+ * Adds a middleware request handler to a router method.
+ */
 export const use = (handler: express.RequestHandler) => {
   return (target: any, key: string) => {
     RouterMeta.get(target.constructor).addMethodMiddleware(key, injector => handler);
   };
 };
 
+/**
+ * Method-level middleware
+ *
+ * Adds a middleware request handler through a provider to a router method.
+ */
 export const useProvider = (provider: MiddlewareProvider) => {
   return (target: any, key: string) => {
     RouterMeta.get(target.constructor).addMethodMiddleware(key, provider);
